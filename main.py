1b9993fbb4ed59e78961f9fa95fccb91b477756d
DamianStef83 <93284248+DamianStef83@users.noreply.github.com>
2021-12-14 20:56:20 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,102 +1,75 @@
-# Damian Stefaniuk
-# Family Cookbook
-# Welcome
-print("Hello, Reader!")
-# Introduction
-print("Welcome to my Family Cook Book")
-print("")  # Separates lines
-print("I am glad you decided to learn my family's christmas traditions!")
-print("")
-# Purpose outlined
-print("Today, we will learn how to make Vareniki.")
-print("")
-# Age
-age=int(input("How old are you?:"))
-if (age>=18):
-      print("This is a delicious dish not only for christmas, but all the"
-            " time!")
-      print("")
-else:
-      print("Starting to cook at a young age is a great thing!")
-      print("")
-# beginning of recipe
-# https://www.196flavors.com/ukraine-vareniki/
-flour = 2  # cups
-eggs = 2
-dMilk = .5   # d is for dough
-dVOil = 2  # tablespoons (Dough)
-salt = 1   # teaspoon
-potato = 1  # lb
-onion = 2
-fMilk = .5  # f is filling
-fVOil = 4  # filling in tablespoons
-butter = 4  # tablespoons
-print("In order to begin your dough, you will need:")
-# using + to include the ingredients for the dough
-print("2 cups Flour, " + "2 Eggs, " + "1/2 cup Milk, "
-      + "2 Tablespoons of Vegetable Oil, " + "and a teaspoon of Salt.")
-print("")
-print("When beginning your process, you will want to combine the flour, eggs,"
-      "milk, and vegetable oil.")
-print("")
-print("Once you have kneaded a nice dough together, cover for 30 minutes, "
-      "but make sure it is away from any heat.")
-print("")
-print("During those 30 minutes, you can take some time off to do anything, "
-      "including this math quiz!")
-quiz=int(input("What is 12 * 3 + 7?"))
-# created a quiz to show I can use basic numeric operators
-ans = (12 * 3 +7)
-if (quiz == ans):
-      print("Nice Job! You aced it!")
-      print("")
-else:
-      print("Looks  like  you failed")
-      print("")
-print("Now that the 30 minutes are up, uncover your dough, and")
-print("cut your dough  into  2 or  3  pieces and roll  out  each piece into"
-      "a thickness of about 1/8th of an inch.")
-print("")
-print("Using a cookie cutter, cut your dough in circles of about 3 inches"
-      "in diameter")
-print("")
-print("Lets go prepare the filling now!")
-print("")
-print("In order to begin your filling, you will need: 1 lb of potatoes, 2 "
-      "onions, 1/2 cup boiling milk, 4 tablespoons vegetable oil, and 4"
-      " tablespoons butter")
-print("")
-print("Start your filling by heating the vegetable oil in  a frying pan and "
-      "fry the onions over medium heat, stirring regularly until golden brown"
-      ", then drain them of their oil and place on plate lined with paper "
-      "towel")
-print("")
-print("Then, you want to peel and boil the potatoes in a large amount of "
-      "slightly salted water")
-print("")
-print("When ready, drain potatoes using a large skimmer and keep the boiling "
-      "water for cooking")
-print("")
-print("Place the cooked potatoes in a large bowl and using a potato masher, "
-      "mash the potato, gradually adding boiling milk. Add the butter and, "
-      "optionally a little boiling water from the potatoes, and mix until "
-      "a slightly firm consistency is obtained.")
-print("")
-print("Follow by adding fried onions and mix. Season with salt and pepper")
-print("")
-print("Now that you have your filling, place 1 teaspoon of filling in the "
-      "center of each circle  of dough, fold them in half and pinch the edges"
-      " with wet  hands.")
-print("")
-print("Now  that you have your raw Vareniki, heat a large abount of boiling"
-      " salt water in a casserole dish")
-print("")
-print("Now, immerse the Vareniki into simmering water and cook for 180 "
-      "seconds. Lets do a countdown!")
-for x in range(180,-1,-1):
-      print(x)
-print("")
-print("Now that your Vareniki are ready, drain the water.")
-print("")
+"""Primary application entrypoint.
+"""
+from __future__ import absolute_import
+
+import locale
+import logging
+import os
+import sys
+
+from pip._internal.cli.autocompletion import autocomplete
+from pip._internal.cli.main_parser import parse_command
+from pip._internal.commands import create_command
+from pip._internal.exceptions import PipError
+from pip._internal.utils import deprecation
+from pip._internal.utils.typing import MYPY_CHECK_RUNNING
+
+if MYPY_CHECK_RUNNING:
+    from typing import List, Optional
+
+logger = logging.getLogger(__name__)
 
 
+# Do not import and use main() directly! Using it directly is actively
+# discouraged by pip's maintainers. The name, location and behavior of
+# this function is subject to change, so calling it directly is not
+# portable across different pip versions.
+
+# In addition, running pip in-process is unsupported and unsafe. This is
+# elaborated in detail at
+# https://pip.pypa.io/en/stable/user_guide/#using-pip-from-your-program.
+# That document also provides suggestions that should work for nearly
+# all users that are considering importing and using main() directly.
+
+# However, we know that certain users will still want to invoke pip
+# in-process. If you understand and accept the implications of using pip
+# in an unsupported manner, the best approach is to use runpy to avoid
+# depending on the exact location of this entry point.
+
+# The following example shows how to use runpy to invoke pip in that
+# case:
+#
+#     sys.argv = ["pip", your, args, here]
+#     runpy.run_module("pip", run_name="__main__")
+#
+# Note that this will exit the process after running, unlike a direct
+# call to main. As it is not safe to do any processing after calling
+# main, this should not be an issue in practice.
+
+def main(args=None):
+    # type: (Optional[List[str]]) -> int
+    if args is None:
+        args = sys.argv[1:]
+
+    # Configure our deprecation warnings to be sent through loggers
+    deprecation.install_warning_logger()
+
+    autocomplete()
+
+    try:
+        cmd_name, cmd_args = parse_command(args)
+    except PipError as exc:
+        sys.stderr.write("ERROR: {}".format(exc))
+        sys.stderr.write(os.linesep)
+        sys.exit(1)
+
+    # Needed for locale.getpreferredencoding(False) to work
+    # in pip._internal.utils.encoding.auto_decode
+    try:
+        locale.setlocale(locale.LC_ALL, '')
+    except locale.Error as e:
+        # setlocale can apparently crash if locale are uninitialized
+        logger.debug("Ignoring error %s when setting locale", e)
+    command = create_command(cmd_name, isolated=("--isolated" in cmd_args))
+
+    return command.main(cmd_args)
